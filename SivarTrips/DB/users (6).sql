9c274d0a5a25d02bea9050b4ba0499cbd53c3df6
Diegople3016 <142236459+Diegople3016@users.noreply.github.com>
2023-08-14 08:13:36 -0600

    Add files via upload

---
+++ SivarTrips/DB/users (6).sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-07-2023 a las 12:05:55
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bdclinicacgm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(11, 'Natanael', '123456'),
+(12, 'Sharon', 'Sharon'),
+(13, 'carla', '123456'),
+(14, 'diego', '122'),
+(15, 'Karla', 'Carlos'),
+(16, 'Karina', 'Karina1'),
+(17, 'Daniela', '123456'),
+(18, 'camila', '1234'),
+(19, 'camila', '1234'),
+(20, 'mauricio', 'mauricio2005'),
+(21, 'nicole', 'hector'),
+(22, 'diego', '1406'),
+(23, 'arturo', '123456'),
+(24, 'carlos', '123456'),
+(25, 'David', 'Davidayala'),
+(26, 'Kathia', 'katty456'),
+(27, 'Claudia', '123456'),
+(28, 'mauricio', 'mauricio');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
